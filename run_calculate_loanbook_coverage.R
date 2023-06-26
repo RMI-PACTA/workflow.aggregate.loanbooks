d70b5ce960d3169acbdd4306d21ad22ccb5ef823
jacobvjk <jacob.kastl@gmail.com>
2023-06-26 11:46:36 +0200

    coverage for all available regions and some minor code improvements

--- run_calculate_loanbook_coverage.R
+++ run_calculate_loanbook_coverage.R
@@ -11,8 +11,12 @@ dotenv::load_dot_env()
 
 # set up project paths----
 if (file.exists(here::here(".env"))) {
+  input_path_scenario <- Sys.getenv("DIR_SCENARIO")
   input_dir_abcd <- Sys.getenv("DIR_ABCD")
   input_path_matched <- Sys.getenv("DIR_MATCHED")
+
+  input_path_regions_geco_2022 <- file.path(input_path_scenario, Sys.getenv("FILENAME_REGIONS_GECO_2022"))
+  input_path_regions_weo_2022 <- file.path(input_path_scenario, Sys.getenv("FILENAME_REGIONS_WEO_2022"))
 
   input_path_abcd <- file.path(input_dir_abcd, Sys.getenv("FILENAME_ABCD"))
 
@@ -29,10 +33,13 @@ abcd <- readr::read_csv(file.path(input_path_abcd))
 # replace potential NA values with 0 in production
 abcd["production"][is.na(abcd["production"])] <- 0
 
+start_year_select <- Sys.getenv("PARAM_START_YEAR")
+
+abcd <- abcd %>%
+  dplyr::filter(.data$year == .env$start_year_select)
+
 
 # coverage of production by companies in loan books compared to total production----
-# matched companies of all production, by sector
-# TODO: this needs to be broken down by region/country
 
 matched_prioritized <- readr::read_csv(
   file.path(input_path_matched, "matched_prio_all_groups.csv")
@@ -44,60 +51,100 @@ matched_prioritized <- readr::read_csv(
 matched_companies <- matched_prioritized %>%
   distinct(name_abcd, sector_abcd, loan_size_outstanding, loan_size_outstanding_currency, score)
 
-# TODO: use current iso bridge
-country_mapping <- r2dii.data::region_isos %>%
-  dplyr::filter(.data$source == "weo_2021")
+scenario_source_input <- Sys.getenv("PARAM_SCENARIO_SOURCE")
+scenario_select <- Sys.getenv("PARAM_SCENARIO_SELECT")
+region_select <- Sys.getenv("PARAM_REGION_SELECT")
+
+regions_geco_2022 <- readr::read_csv(input_path_regions_geco_2022)
+regions_weo_2022 <- readr::read_csv(input_path_regions_weo_2022)
+
+# get required countries for region_select----
+region_isos_complete <- r2dii.data::region_isos %>%
+  rbind(regions_geco_2022) %>%
+  rbind(regions_weo_2022)
+
+region_isos_select <- region_isos_complete %>%
+  dplyr::filter(
+    .data$source == .env$scenario_source_input
+  )
 
 # available_regions
-unique(country_mapping$region)
+available_regions <- unique(region_isos_select$region)
 
-# region_select <- "european union"
-region_select <- "global"
-countries_select <- country_mapping %>%
-  dplyr::filter(.data$region == .env$region_select) %>%
-  dplyr::pull(.data$isos) %>%
-  toupper()
+# calculate summary stats for each available region----
+production_coverage_summary <- NULL
 
+for (region_i in available_regions) {
+  countries_select_i <- region_isos_select %>%
+    dplyr::filter(.data$region == .env$region_i) %>%
+    dplyr::pull(.data$isos) %>%
+    toupper()
 
-# production_coverage_companies <- abcd %>%
-production_coverage_companies <- abcd %>%
-  dplyr::filter(
-    .data$year == 2022,
-    .data$plant_location %in% .env$countries_select
-  ) %>%
-  dplyr::summarise(
-    emission_factor = stats::weighted.mean(.data$emission_factor, w = .data$production, na.rm = TRUE),
-    production = sum(.data$production, na.rm = TRUE),
-    .by = c(
-      "company_id", "name_company", "lei", "is_ultimate_owner", "sector",
-      "technology", "year", "production_unit", "emission_factor_unit", "ald_timestamp"
+  # summarise abcd by relevant region
+  production_coverage_summary_i <- abcd %>%
+    dplyr::filter(.data$plant_location %in% .env$countries_select_i) %>%
+    dplyr::summarise(
+      emission_factor = stats::weighted.mean(.data$emission_factor, w = .data$production, na.rm = TRUE),
+      production = sum(.data$production, na.rm = TRUE),
+      .by = c(
+        "company_id", "name_company", "lei", "is_ultimate_owner", "sector",
+        "technology", "year", "production_unit", "emission_factor_unit", "ald_timestamp"
+      )
     )
-  ) %>%
-  dplyr::left_join(
-    matched_companies,
-    by = c("name_company" = "name_abcd", "sector" = "sector_abcd")
+
+  # add information on matched companies across analysed loan books
+  production_coverage_summary_i <- production_coverage_summary_i %>%
+    dplyr::left_join(
+      matched_companies,
+      by = c("name_company" = "name_abcd", "sector" = "sector_abcd")
+    )
+
+  # calculate summary statistics
+  production_coverage_summary_i <- production_coverage_summary_i %>%
+    dplyr::mutate(
+      financed_production = dplyr::if_else(.data$score == 1, .data$production, 0),
+      matched_company = dplyr::if_else(.data$score == 1, .data$name_company, NA_character_)
+    ) %>%
+    dplyr::mutate(
+      matched_rows_company_sector = sum(.data$score, na.rm = TRUE),
+      .by =c("name_company", "sector")
+    ) %>%
+    dplyr::summarise(
+      total_exposure = sum(loan_size_outstanding / matched_rows_company_sector, na.rm = TRUE),
+      n_companies_matched = dplyr::n_distinct(.data$matched_company, na.rm = TRUE),
+      n_companies_total = dplyr::n_distinct(.data$name_company, na.rm = TRUE),
+      production_financed = sum(.data$financed_production, na.rm = TRUE),
+      production_total = sum(.data$production, na.rm = TRUE),
+      .by = c("sector")
+    ) %>%
+    dplyr::mutate(
+      share_companies_matched = .data$n_companies_matched / .data$n_companies_total,
+      share_production_financed = .data$production_financed / .data$production_total,
+      region = .env$region_i
+    )
+
+  production_coverage_summary <- production_coverage_summary %>%
+    bind_rows(production_coverage_summary_i)
+
+}
+
+# format
+production_coverage_summary <- production_coverage_summary %>%
+  select(
+    c(
+      "region",
+      "sector",
+      "total_exposure",
+      "n_companies_matched",
+      "n_companies_total",
+      "share_companies_matched",
+      "production_financed",
+      "production_total",
+      "share_production_financed"
+    )
   )
 
-production_coverage_summary <- production_coverage_companies %>%
-  dplyr::mutate(
-    financed_production = dplyr::if_else(.data$score == 1, .data$production, 0),
-    matched_company = dplyr::if_else(.data$score == 1, .data$name_company, NA_character_)
-  ) %>%
-  dplyr::mutate(
-    matched_rows_company_sector = sum(.data$score, na.rm = TRUE),
-    .by =c("name_company", "sector")
-  ) %>%
-  dplyr::summarise(
-    total_exposure = sum(loan_size_outstanding / matched_rows_company_sector, na.rm = TRUE),
-    matched_number_companies = dplyr::n_distinct(.data$matched_company, na.rm = TRUE),
-    matched_number_companies = dplyr::n_distinct(.data$matched_company, na.rm = TRUE),
-    total_number_companies = dplyr::n_distinct(.data$name_company, na.rm = TRUE),
-    total_production = sum(.data$production, na.rm = TRUE),
-    financed_production = sum(.data$financed_production, na.rm = TRUE),
-    .by = c("sector")
-  ) %>%
-  dplyr::mutate(
-    share_matched_companies = .data$matched_number_companies / .data$total_number_companies,
-    share_financed_production = .data$financed_production / .data$total_production
-  )
+# save to matched directory
+production_coverage_summary %>%
+  readr::write_csv(file.path(input_path_matched, "summary_statistics_loanbook_coverage.csv"))
 
